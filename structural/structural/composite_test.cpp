5d24740bd2f72da85e208878be45812c9fc42559
Adam <adam.ghandoura@gmail.com>
2016-09-30 23:00:34 +0100

    add builder method for test

--- structural/structural/composite_test.cpp
+++ structural/structural/composite_test.cpp
@@ -8,34 +8,29 @@
 //a + (b + c)
 //this can be optimized using google test framework
 //todo look it up
-struct addition_test_3
+AdditionExper build_addition_test_3(const double &a, const double &b, const double &c)
 {
-    AdditionExper expr;
-    addition_test_3(const double &a, const double &b, const double &c)
-        :expr{
-            std::make_shared<Literal>(Literal(a)),
+    return{ std::make_shared<Literal>(Literal(a)),
             std::make_shared<AdditionExper>(
-                std::make_shared<Literal>(b),
-                std::make_shared<Literal>(c))
-    }
-    {}
-    
+               std::make_shared<Literal>(b),
+               std::make_shared<Literal>(c))
+    };
 };
 
 TEST(composition_test, eval)
 {
-    addition_test_3 sum3(1, 2, 3);
+    auto sum3 = build_addition_test_3(1, 2, 3);
 
-    EXPECT_DOUBLE_EQ(sum3.expr.eval(), 6);
+    EXPECT_DOUBLE_EQ(sum3.eval(), 6);
 }
 
 TEST(composition_test, collect)
 {
-    addition_test_3 sum3(1.3, 2.3, 3);
+    auto sum3 = build_addition_test_3(1.3, 2.3, 3);
 
     std::vector<double> v;
 
-    sum3.expr.collect(v);
+    sum3.collect(v);
 
     EXPECT_DOUBLE_EQ(v[0], 1.3);
     EXPECT_DOUBLE_EQ(v[1], 2.3);
